0517e88c976e489a7d57cef36359491cb272fc26
Zhaojiang Chang <zhaojiangchangnz@gmail.com>
2015-06-12 16:30:45 +1200

    load all files on load

---
+++ index.js
@@ -0,0 +1,210 @@
+
+
+function getYear(y){
+  year = y;
+  trans_ladder(year);
+  return false;
+}
+
+
+function ladder_table(year) {
+  document.getElementById("chart_text").innerHTML = year+" ranking: </br>";
+  var index = parseInt(year)-2008;
+  ts = years_teams[index].teams;
+          var rect = svg.selectAll("rect")
+              .data(ts)
+              .enter()
+              .append("rect")
+              .attr("x", 0)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50
+                })
+              .attr("width", width)
+              .attr("height", 50)
+              .attr("fill", "#191B25")
+              .on("mouseover", function(d) {
+                  d3.select(this)
+                  .attr("fill", function(d) {
+                      if(d.ranking === 1){
+                        return "#ED008C"
+                      }
+                      else{
+                        return "#052F6B"
+                      }
+                    })
+                })
+              .on("mouseout", function () {
+                   d3.select(this)
+                   .attr("fill", "#191B25")
+                 })
+
+
+        var test_bar = svg.selectAll("text")
+              .data(ts)
+              .enter()
+              .append("g")
+
+            test_bar.append("text")
+              .style("font-weight", "bold")
+              .attr("x", 20)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.ranking
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", width/6)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.coun
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", width / 2 -100)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.name
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+            test_bar.append("text")
+              .attr("x", (width / 5)*3 +50)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.win
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", (width / 5)*4 )
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.lost
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", width - 50)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.regular_final_score
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+          }
+
+      function trans_ladder(year){
+        document.getElementById("chart_text").innerHTML = year+" ranking: </br>";
+        var index = parseInt(year)-2008;
+        ts = years_teams[index].teams;
+          var bars = svg.selectAll("rect")
+                .data(ts)
+
+          bars.transition()
+            .delay(function(d, i) {
+              return i * 30
+              })
+            .duration(500)
+            .each("start", function(){
+                d3.select(this)
+                  .attr("x", width)
+                })
+            .attr("x", 0)
+            .attr("y", function(d, i) {
+                return (i+1) * 50
+              })
+            .attr("width", width)
+            .attr("height", 50)
+            .attr("fill", "#191B25");
+
+        svg.selectAll("g")
+              .remove()
+        var test_bar = svg.selectAll("text")
+              .data(ts)
+              .enter()
+              .append("g")
+
+            test_bar.append("text")
+              .attr("x", 20)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.ranking
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", width/6)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.coun
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", width/2-100)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.name
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+            test_bar.append("text")
+              .attr("x", (width / 5)*3 +50)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.win
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+            test_bar.append("text")
+              .attr("x", (width / 5)*4 )
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.lost
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+
+
+            test_bar.append("text")
+              .attr("x", width - 50)
+              .attr("y", function(d, i) {
+                  return (i+1) * 50 + 20
+                })
+              .text(function(d) {
+                  return d.regular_final_score
+                  })
+              .attr("font-size", "15")
+              .attr("fill", "white")
+      }
---
+++ loadFiles.js
@@ -0,0 +1,108 @@
+var years_teams =  new Array();
+var teamInfo = new Array();
+var remaining = 6;
+//var j = 0;
+//console.log("aaa: " + years_teams.length);
+
+//for(j = 0; j<years_teams.length; j++){
+
+	 //  if(years_teams[j].year.localeCompare(year)){
+		//   console.log("aaaaaaa")
+		//   teamInfo = years.teams[j].teams
+	  // }
+	  // console.log("bbbbbb")
+
+//}s
+function read_data() {
+    var years = new Array("2008","2009","2010","2011","2012","2013");
+    var count = 0;
+
+    d3.csv("2008" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+	if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2008");
+	});
+
+    d3.csv("2009" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+	if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2009");
+	});
+
+    d3.csv("2010" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+	if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2010");
+	});
+
+	d3.csv("2011" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+	if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2011");
+	});
+
+    d3.csv("2012" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+    if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2012");
+	});
+
+    d3.csv("2013" + "_new_Table1.csv", function(data) {
+	init_team_list();
+	init_basic_info(data);
+	team_list.sort(function(a, b) {
+			return a.ranking - b.ranking
+	})
+	if(!--remaining) myLadderFunction();
+	years_teams[count++] = new year_teams("2013");
+	});
+}
+function year_teams(year){
+    //console.log(year);
+
+  this.year = year;
+  this.teams = myFunction();
+ // console.log(this.teams);
+ // this.getYear = function(){
+  //  return ths.year
+   // }
+  //this.getTeam_list = function(){
+   // return this.teams
+   // }
+  //this.getTeamByName = funcion(teamName){
+   // return getByName(teamName);
+  //}
+ // function getByName(teamName){
+   // var i = 0;
+   // for(i=0; i<this.teams.lenght; i++){
+    //  if(this.teams[i].name == teamName)(
+	//return this.teams[i];
+   // }
+ // }
+}
+
+function myLadderFunction(){
+	 initial();
+	 ladder_table(year);
+}
+
